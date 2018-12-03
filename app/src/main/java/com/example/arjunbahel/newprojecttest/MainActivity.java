81633483434cb3500219da3443f08d78b68078af
Chaitanya Sood <chaitanyasood01@gmail.com>
2018-12-03 12:41:40 -0600

    Change permissions from settings, added textview

--- app/src/main/java/com/example/arjunbahel/newprojecttest/MainActivity.java
+++ app/src/main/java/com/example/arjunbahel/newprojecttest/MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.arjunbahel.newprojecttest;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.graphics.Path;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+
+    private boolean checkPermission() {
+
+//Check for READ_EXTERNAL_STORAGE access, using ContextCompat.checkSelfPermission()//
+
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+//If the app does have this permission, then return true//
+
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+
+//If the app doesn’t have this permission, then return false//
+
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
     private static int RESULT_LOAD_IMG = 1;
     String imgDecodableString;
     @Override
@@ -21,16 +52,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void loadImagefromGallery(View view) {
         // Create intent to Open Image applications like Gallery, Google Photos
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         // Start the Intent
         startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (!checkPermission()) {
+            Toast.makeText(this, "Go to the settings and enable storage access",
+                    Toast.LENGTH_LONG).show();
+        }
         try {
             // When an Image is picked
             if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
